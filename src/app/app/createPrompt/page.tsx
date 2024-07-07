9a144d37ae39caa659ba364331e867f28665a26b
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2024-07-07 03:30:04 -0400

    spacing refinements

--- src/app/app/createPrompt/page.tsx
+++ src/app/app/createPrompt/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
   const { user } = useAuth();
 
   return (
-    <div className="w-screen h-screen p-8 pt-28 flex gap-24 justify-center overflow-auto">
+    <div className="w-full h-full p-8 pt-28 flex gap-24 justify-center overflow-auto">
       <div className="flex flex-col gap-8 w-full">
         <NewPromptForm userId={user?.id} />
       </div>
--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
   const { user } = useAuth();
 
   return (
-    <div className="w-screen h-full p-8 pt-28 flex gap-24 justify-center">
+    <div className="w-full h-full p-8 pt-28 flex gap-24 justify-center">
       {user && (
         <div className="flex flex-col gap-4 w-full h-full items-center justify-center text-xl font-bold text-center ">
           Create a new prompt to get started
