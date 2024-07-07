8e8b3129c497f492bedc8c771b8a9ada5ec4e9d4
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2024-07-06 22:50:49 -0400

    Refactored login

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -5,22 +5,18 @@ import { redirect } from "next/navigation";
 import { supabase } from "@/utils/supabase/server";
 import { NextRequest, NextResponse } from "next/server";
 
-async function googleLogin() {
-  const { data, error } = await supabase.auth.signInWithOAuth({
-    provider: "google",
-    options: {
-      redirectTo: "/auth/callback",
-    },
-  });
-
-  if (data.url) {
-    redirect(data.url); // use the redirect API for your server framework
-  }
-}
-
 export async function POST(req: NextRequest) {
   try {
-    googleLogin();
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: "/auth/callback",
+      },
+    });
+
+    if (data.url) {
+      redirect(data.url); // use the redirect API for your server framework
+    }
 
     return NextResponse.json({ message: "success" }, { status: 200 });
   } catch (error: any) {
