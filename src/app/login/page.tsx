718712e0539f7776e5667542baee0bcce8e29fa5
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2024-07-07 02:43:04 -0400

    refined login and signup

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,25 +1,34 @@
+"use client";
 import TextInput from "@/components/textInput";
 import Button from "@/components/button";
 import { FaGoogle, FaGithub } from "react-icons/fa";
+import { GoogleLogin, GitHubLogin } from "@/helpers/sso";
 
 export default function Page() {
+  const loginWithGoogle = async () => {
+    GoogleLogin();
+  };
+
+  const loginWithGithub = async () => {
+    GitHubLogin();
+  };
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center gap-4">
+    <div className="w-screen h-screen flex flex-col justify-center items-center gap-4 p-4">
       <h2 className="text-4xl font-bold">Sign In</h2>
-      <div className="w-fit border-2 dark:border-white border-black rounded-xl flex flex-col justify-center items-center p-8 gap-4 ">
-        {/* <TextInput classNames="w-full" placeholder="Email address..." />
-        <TextInput classNames="w-full" placeholder="Password..." />
-        <div className="w-full">
-          <Button className="dark:bg-white bg-black dark:text-black  text-white w-full">
-            Sign In
+      <div className="w-fit border-2 border-black dark:border-white rounded-xl flex flex-col justify-center items-center p-4 gap-4 ">
+        <div className="grid grid-cols-2 grid-flow-col auto-cols-fr gap-4 text-sm md:text-lg w-full">
+          <Button
+            onClick={loginWithGoogle}
+            className="w-full flex justify-center items-center gap-2 p-4 "
+          >
+            <FaGoogle className="text-3xl" />
+            Sign In With Google
           </Button>
-        </div> */}
-        <div className="grid grid-cols-2 grid-flow-col auto-cols-fr gap-4 text-lg w-full">
-          <Button className="w-full flex justify-center items-center gap-2 p-4">
-            <FaGoogle /> Sign In With Google
-          </Button>
-          <Button className="w-full  flex justify-center items-center gap-2 px-8">
-            <FaGithub />
+          <Button
+            onClick={loginWithGithub}
+            className="w-full flex justify-center items-center gap-2 p-4 "
+          >
+            <FaGithub className="text-3xl" />
             Sign In With GitHub
           </Button>
         </div>
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -14,37 +14,22 @@ export default function Page() {
   };
 
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center gap-4">
+    <div className="w-screen h-screen flex flex-col justify-center items-center gap-4 p-4">
       <h2 className="text-4xl font-bold">Sign Up</h2>
-      <div className="w-fit border-2 border-black dark:border-white rounded-xl flex flex-col justify-center items-center p-8 gap-4 ">
-        {/* <div className="flex gap-4 w-full">
-          <TextInput classNames="w-full" placeholder="First Name..." />
-          <TextInput classNames="w-full" placeholder="Last Name..." />
-        </div>
-
-        <TextInput classNames="w-full" placeholder="Email address..." />
-        <TextInput classNames="w-full" placeholder="Password..." />
-        <div className="w-full">
-          <Button
-            onClick={signUp}
-            className="bg-black dark:bg-white text-white dark:text-black w-full"
-          >
-            Sign Up
-          </Button>
-        </div> */}
-        <div className="grid grid-cols-2 grid-flow-col auto-cols-fr gap-4 text-lg w-full">
+      <div className="w-fit border-2 border-black dark:border-white rounded-xl flex flex-col justify-center items-center p-4 gap-4 ">
+        <div className="grid grid-cols-2 grid-flow-col auto-cols-fr gap-4 text-sm md:text-lg w-full">
           <Button
             onClick={loginWithGoogle}
-            className="w-full flex justify-center items-center gap-2 p-4"
+            className="w-full flex justify-center items-center gap-2 p-4 "
           >
-            <FaGoogle />
+            <FaGoogle className="text-3xl" />
             Sign Up With Google
           </Button>
           <Button
             onClick={loginWithGithub}
-            className="w-full  flex justify-center items-center gap-2 px-8"
+            className="w-full flex justify-center items-center gap-2 p-4 "
           >
-            <FaGithub />
+            <FaGithub className="text-3xl" />
             Sign Up With GitHub
           </Button>
         </div>
