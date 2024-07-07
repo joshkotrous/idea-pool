1cd215030542fbe42dc9c39c2e77438502cb06db
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2024-07-07 00:59:31 -0400

    Removed create new prmopt message if not logged in

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -2,16 +2,20 @@
 
 import Button from "@/components/button";
 import Link from "next/link";
-
+import { useAuth } from "@/utils/supabase/authProvider";
 export default function Page() {
+  const { user } = useAuth();
+
   return (
     <div className="w-screen h-screen p-8 pt-28 flex gap-24 justify-center">
-      <div className="flex flex-col gap-4 w-full h-full items-center justify-center text-xl font-bold">
-        Create a new prompt to get started
-        <Link href="/app/createPrompt">
-          <Button>Create Prompt</Button>
-        </Link>
-      </div>
+      {user && (
+        <div className="flex flex-col gap-4 w-full h-full items-center justify-center text-xl font-bold">
+          Create a new prompt to get started
+          <Link href="/app/createPrompt">
+            <Button>Create Prompt</Button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
